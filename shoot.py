6e9ab7de6acf62e76c14121f181288a0b9158deb
the-real-finnventor <71413483+the-real-finnventor@users.noreply.github.com>
2023-04-11 19:06:09 -0400

    Add files via upload

---
+++ shoot.py
@@ -0,0 +1,20 @@
+from random import *
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+    else:
+        print("You lose")
+        quit()
+    
+place_apple()
