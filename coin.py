cfbae4b60c87442ac82691bbfd0d9e857457ccc9
the-real-finnventor <71413483+the-real-finnventor@users.noreply.github.com>
2023-04-11 18:48:41 -0400

    Add files via upload

---
+++ coin.py
@@ -0,0 +1,48 @@
+from random import *
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+fox = Actor("fox")
+fox.pos = 200, 200
+coin = Actor("coin")
+coin.pos = 200, 200
+
+def draw():
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+    screen.draw.text(f"Score: {str(score)}", color="black", topleft=(10, 10))
+
+    if game_over == True:
+        screen.fill("pink")
+        screen.draw.text(f"Score: {str(score)}", topleft=(10, 10), fontsize=60)
+
+def place_coin():
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT -20))
+
+def time_up():
+    global game_over
+    game_over = True
+
+def update():
+    global score
+
+    if keyboard.left:
+        fox.x = fox.x - 4
+    elif keyboard.right:
+        fox.x = fox.x + 4
+    elif keyboard.up:
+        fox.y = fox.y - 4
+    elif keyboard.down:
+        fox.y = fox.y + 4
+    
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        score += 10
+        place_coin()
+
+clock.schedule(time_up, 16.0)
+place_coin()
